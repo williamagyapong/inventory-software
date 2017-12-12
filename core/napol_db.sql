3417dbe4ce5fe20b889c2900f20d6dc6b98b11e7
William Ofosu Agyapong <willisco413@gmail.com>
2017-12-12 16:39:14 +0000

    Updated database

--- core/napol_db.sql
+++ core/napol_db.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2017 at 02:40 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Dec 12, 2017 at 05:36 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -39,8 +41,8 @@ CREATE TABLE `materials` (
 --
 
 INSERT INTO `materials` (`id`, `name`, `quantity`, `unit`, `date_added`) VALUES
-(1, 'Iron rods', 1250, '', '0000-00-00'),
-(2, 'Roofing sheets', 662, 'pieces', '0000-00-00'),
+(1, 'Iron rods', 1000, '', '0000-00-00'),
+(2, 'Roofing sheets', 674, 'pieces', '0000-00-00'),
 (3, 'cement', 1000, 'bags', '0000-00-00'),
 (4, 'sand', 215, '', '0000-00-00'),
 (6, 'gravels', 100, 'trips', '0000-00-00'),
@@ -50,7 +52,20 @@ INSERT INTO `materials` (`id`, `name`, `quantity`, `unit`, `date_added`) VALUES
 (11, 'wall tiles', 0, '', '0000-00-00'),
 (13, 'sandcrete', 0, '', '0000-00-00'),
 (15, 'plywood', 0, '', '0000-00-00'),
-(16, 'Door frame', 0, '', '2017-09-21');
+(16, 'Door frame', 50, '', '2017-09-21'),
+(17, 'tank', 0, '', '2017-11-21'),
+(24, 'this material', 0, '', '2017-11-24'),
+(25, 'new one', 400, 'new unit', '2017-11-24'),
+(26, 'aSDFG', 0, 'SASA', '2017-11-24'),
+(27, 'matatata', 123, 'this unit', '2017-11-24'),
+(28, 'wertrturt', 60, 'true', '2017-11-24'),
+(29, 'my my', 60, 'true', '2017-11-24'),
+(30, 'asdfdgfhg', 60, '1234', '2017-11-24'),
+(31, 'aaaa', 1000, 'uuuu', '2017-11-24'),
+(32, 'kkkkkkk', 134, 'ddd', '2017-11-24'),
+(33, 'ttttttt', 56, 'ssss', '2017-11-24'),
+(34, 'asdad', 0, 'assdfdas', '2017-11-28'),
+(35, 'wqreww', 0, 'awaw', '2017-12-02');
 
 -- --------------------------------------------------------
 
@@ -79,8 +94,9 @@ INSERT INTO `material_flow` (`id`, `material_id`, `quantity_received`, `quantity
 (2, 1, 50, 0, '2017-09-20', '', '', '', 0),
 (3, 2, 12, 0, '2017-09-21', '', '', '', 0),
 (4, 4, 15, 0, '2017-09-21', '', '', '', 0),
-(5, 1, 0, 20, '2017-09-22', 'For construction of project', 'Materials Department', '{"name":"William Amanor","position":"Project Supervisor"}', 6),
-(6, 2, 0, 30, '2017-09-22', 'For construction of project', 'Materials Department', '{"name":"William Amanor","position":"Project Supervisor"}', 6);
+(5, 1, 0, 20, '2017-09-22', 'For construction of project', 'Materials Department', '{\"name\":\"William Amanor\",\"position\":\"Project Supervisor\"}', 6),
+(6, 2, 0, 30, '2017-09-22', 'For construction of project', 'Materials Department', '{\"name\":\"William Amanor\",\"position\":\"Project Supervisor\"}', 6),
+(7, 2, 12, 0, '2017-12-02', '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -115,22 +131,23 @@ CREATE TABLE `projects` (
   `stores_admin` text NOT NULL,
   `status` varchar(10) NOT NULL DEFAULT '0',
   `bill_status` varchar(10) NOT NULL DEFAULT 'none',
-  `d_notes` text NOT NULL
+  `d_notes` text NOT NULL,
+  `date_registered` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`id`, `name`, `description`, `location`, `date_begun`, `date_completion`, `company_in_charge`, `project_manager`, `stores_admin`, `status`, `bill_status`, `d_notes`) VALUES
-(1, 'sadfg', 'fvgnm', 'dfgh', '2017-09-16', '2018-04-26', 'dfgh', '{"name":"bbbbbbb","phone":"2222222222"}', '{"name":"sssssssssssss","phone":"3333333333"}', '0', 'none', ''),
-(2, 'A lecture theatre for the department of Agric science, KNUST', 'Five storey lecture theatre', 'KNUST', '2017-06-05', '2018-03-21', 'Nadaco construction Limited', '{"name":"Agyemang Prempeh","phone":"024567854"}', '{"name":"Adakwah Joseph","phone":"0234125345"}', '3', 'none', 'Please call the project manager for changes'),
-(3, 'Four storey classroom block for SDA SHS, Tarkwah Breman', 'dddddddddddddd', 'ccccccccc', '2017-09-02', '2018-05-02', 'bbbbbb', '{"name":"Agyemang Sampson","phone":"0245657890"}', '{"name":"Frimpong Mandy","phone":"0234125345"}', '1', '1', ''),
-(4, 'qeqwewrewtwty ewrtty  trghhyj reth', 'ewrtgr5y ergtrtrtyh retryht rtytr', 'erty ertyert', '2017-09-07', '2018-04-11', 'ertyuisdfgh erty', '{"name":"sdfvcgnh dsfghtj","phone":"0234556546"}', '{"name":"dfhgfjhfd fdgf","phone":"0257474848"}', '1', '3', 'what is wrong'),
-(5, 'sfhhghtgj fhgff', 'dfghfghngfjg', 'fdghgfjhgjkghk', '2017-09-07', '2017-12-31', 'dffdghhgfh', '{"name":"erftyryujy","phone":"0234556546"}', '{"name":"defgerfghyhj","phone":"0234125345"}', '1', '0', ''),
-(6, 'Five star hotel building', 'rredfdssdg rgredged', 'ertgryjhtykt', '2017-09-07', '2017-12-26', 'Access Bank Ghana', '{"name":"esdgfghbtfygh","phone":"0234556546"}', '{"name":"asfsdgdf","phone":"0257474848"}', '1', '1', ''),
-(7, 'ssssssssssssss', 'dgfhbjk,l', 'fghjk,l.', '2017-09-09', '2018-01-19', 'dfghmj,kl', '{"name":"asfderghjkl","phone":"0245657890"}', '{"name":"sdfghjkldf","phone":"0234562365"}', '1', 'none', ''),
-(8, '1000 kilometers dual carriage road', '1000 kilometers dual carriage linking Accra and Cape Coast', 'Accra -Cape Coast highway', '2017-09-21', '2018-09-20', 'Jackson construction Limited', '{"name":"Asane Frimpong","phone":"0234556546"}', '{"name":"Ameyaw Dennis","phone":"0257474848"}', '0', 'none', 'Please check the location and add road to the project name');
+INSERT INTO `projects` (`id`, `name`, `description`, `location`, `date_begun`, `date_completion`, `company_in_charge`, `project_manager`, `stores_admin`, `status`, `bill_status`, `d_notes`, `date_registered`) VALUES
+(1, 'Odorna Bridge', 'Bridge over the odorna lagoon', 'Accra', '2017-09-16', '2018-04-26', 'Geroz Construction', '{\"name\":\"Daniel Asiamah\",\"phone\":\"0343434343\"}', '{\"name\":\"Isaac Boakye\",\"phone\":\"0121313212\"}', '0', 'none', '', '2017-08-08'),
+(2, 'A lecture theatre for the department of Agric science, KNUST', 'Five storey lecture theatre', 'KNUST', '2017-06-05', '2018-03-21', 'Nadaco construction Limited', '{\"name\":\"Agyemang Prempeh\",\"phone\":\"0245678545\"}', '{\"name\":\"Pasty Asamoah\",\"phone\":\"0234125345\"}', '3', 'none', 'Please note that talks on the site and manager of this project has not yet been finalised.', '2017-08-09'),
+(3, 'Four storey classroom block for SDA SHS, Ahodwo', 'Four storey classroom block', 'Ahodwo, Kumasi', '2017-09-02', '2018-05-02', 'T & G construction company LTD', '{\"name\":\"Agyemang Sampson\",\"phone\":\"0245657890\"}', '{\"name\":\"Frimpong Mandy\",\"phone\":\"0234125345\"}', '1', '3', 'This is a sample message from the manager', '2017-08-19'),
+(4, 'Accra to Kumasi dual carriage road', 'A highway linking Accra and Kumasi', 'Accra - Kumasi road, Ghana', '2017-09-07', '2018-04-11', 'Geostruct Company LTD', '{\"name\":\"Osafo Douglas\",\"phone\":\"0234556546\"}', '{\"name\":\"Agyemang Badu\",\"phone\":\"0257474848\"}', '1', '2', '', '2017-08-20'),
+(5, 'Kejetia Market', 'Ultra modern  market in  the Kumasi Metropolis', 'Kumasi, Ashanti Region', '2017-09-07', '2017-12-31', 'Veep construction ', '{\"name\":\"erftyryujy\",\"phone\":\"0234556546\"}', '{\"name\":\"defgerfghyhj\",\"phone\":\"0234125345\"}', '1', '3', 'Please add 200 door frames to the list', '2017-08-28'),
+(6, 'Five star hotel building', 'rredfdssdg rgredged', 'ertgryjhtykt', '2017-09-07', '2017-12-26', 'Access Bank Ghana', '{\"name\":\"esdgfghbtfygh\",\"phone\":\"0234556546\"}', '{\"name\":\"asfsdgdf\",\"phone\":\"0257474848\"}', '1', '1', '', '2017-08-29'),
+(7, 'Kwame Nkrumah Circle over pass', '500 Kilometers overpass', 'Circle, Greater-Accra', '2017-09-09', '2018-01-19', 'Geroz Construction', '{\"name\":\"asfderghjkl\",\"phone\":\"0245657890\"}', '{\"name\":\"sdfghjkldf\",\"phone\":\"0234562365\"}', '1', 'none', '', '2017-09-03'),
+(8, '1000 kilometers dual carriage road', '1000 kilometers dual carriage linking Accra and Cape Coast', 'Accra -Cape Coast highway', '2017-09-21', '2018-09-20', 'Jackson construction Limited', '{\"name\":\"Asane Frimpong\",\"phone\":\"0234556546\"}', '{\"name\":\"Ameyaw Dennis\",\"phone\":\"0257474848\"}', '1', 'none', '', '2017-09-05');
 
 -- --------------------------------------------------------
 
@@ -155,16 +172,18 @@ INSERT INTO `project_material` (`id`, `project_id`, `material_id`, `quantity_nee
 (2, 6, 1, 100, '0000-00-00'),
 (3, 6, 4, 10, '0000-00-00'),
 (4, 6, 6, 5, '0000-00-00'),
-(5, 3, 13, 100, '0000-00-00'),
-(6, 3, 2, 100, '0000-00-00'),
-(7, 4, 4, 5, '0000-00-00'),
-(8, 4, 2, 30, '0000-00-00'),
-(9, 4, 13, 15, '0000-00-00'),
-(10, 5, 2, 20000, '0000-00-00'),
-(11, 5, 11, 12, '0000-00-00'),
-(12, 5, 15, 345, '0000-00-00'),
-(13, 5, 7, 700, '0000-00-00'),
-(14, 5, 13, 15, '0000-00-00');
+(5, 3, 13, 100, '2017-10-04'),
+(6, 3, 2, 100, '2017-10-04'),
+(7, 4, 4, 5, '2017-10-18'),
+(8, 4, 2, 30, '2017-10-18'),
+(9, 4, 13, 15, '2017-10-18'),
+(10, 5, 2, 20000, '2017-11-09'),
+(11, 5, 11, 12, '2017-11-09'),
+(12, 5, 15, 345, '2017-11-09'),
+(13, 5, 7, 700, '2017-11-09'),
+(14, 5, 13, 15, '2017-11-09'),
+(23, 7, 2, 23, '2017-11-24'),
+(24, 7, 11, 67, '2017-11-24');
 
 -- --------------------------------------------------------
 
@@ -237,32 +256,39 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `materials`
 --
 ALTER TABLE `materials`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
 --
 -- AUTO_INCREMENT for table `material_flow`
 --
 ALTER TABLE `material_flow`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `material_returns`
 --
 ALTER TABLE `material_returns`
   MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `project_material`
 --
 ALTER TABLE `project_material`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
